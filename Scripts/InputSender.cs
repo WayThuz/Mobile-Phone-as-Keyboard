0569c9b7f25f1ccaf6de7553e2c6a77b918b61cf
WayThuz <87470810+WayThuz@users.noreply.github.com>
2024-03-25 18:21:02 +0800

    Add files via upload

---
+++ Scripts/InputSender.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class InputSender : MonoBehaviour {
+
+    public void OnInputX() {
+
+    }
+}
